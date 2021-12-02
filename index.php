77fbc74cd91ea91d4ac0a25da3261ae95eed3daf
xizzana <xizanishvili.99@gmail.com>
2021-12-03 01:44:43 +0400

    initial code

--- index.php
+++ index.php
@@ -17,17 +17,17 @@
 <body>
     <?php include 'data.php' ?>
 
-    <header class="sticky-top">
-        <nav class="container navbar navbar-expand-lg navbar-dark">
+    <header class="fixed-top">
+        <nav class="container navbar navbar-expand-md navbar-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">MyEshop</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
+                    <ul class="navbar-nav d-flex justify-content-between ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Products</a>
